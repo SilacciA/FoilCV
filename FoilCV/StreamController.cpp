87ab9cd8cabbd790f695c2a15e9a8a302d7e13b9
Alessandro Silacci <silacci.alessandro@gmail.com>
2017-11-10 10:56:42 +0100

    Removed misplaced files.

--- FoilCV/StreamController.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  StreamController.cpp
-//  FoilCV
-//
-//  Created by Alessandro Silacci on 07.11.17.
-//  Copyright © 2017 Alessandro Silacci. All rights reserved.
-//
-
-#include "StreamController.hpp"
